0335530b437d869ab05814d951c57df8ab9fae95
Mukul Nanda <90479242+muknan@users.noreply.github.com>
2026-07-28 15:32:37 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+    //   if(s.length!=t.length) return false;
+    //   const res = [...s].sort().join('') === [...t].sort().join('');
+    //   return res;
+        if(s.length!=t.length) return false;
+        const count = {};
+
+        for (const char of s){
+            count[char] = ((count[char] || 0) + 1);
+        }
+
+        for (const char of t){
+            if(!count[char]) return false;
+            count[char]--;
+        }
+        return true;
+    }
+}
