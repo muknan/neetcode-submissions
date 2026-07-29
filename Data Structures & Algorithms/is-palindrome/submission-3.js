e9f0dc78526f02a1be3126e4711b1e709197fb33
Mukul Nanda <90479242+muknan@users.noreply.github.com>
2026-07-29 13:32:24 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isAlphaNumeric(c){
+        return ((c>='a'&&c<='z') || (c>='A'&&c<='Z') || (c>='0'&&c<='9'));
+    }
+    isPalindrome(s) {
+        // const str = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+        let str='';
+        for(let c of s){
+            if(this.isAlphaNumeric(c)){
+                str+=c.toLowerCase();
+            }
+        }
+
+        let start = 0;
+        let end = str.length-1;
+
+        while(start<=end){
+            if(str[start]===str[end]){
+                start++;
+                end--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
