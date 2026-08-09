02643d8d1198cde21236974630c846c9848eb841
Mukul Nanda <90479242+muknan@users.noreply.github.com>
2026-08-09 13:13:54 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = ''
+        for (const s of strs){
+            res += s.length + '#' + s
+        }
+        return res
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const res = []
+        let i=0
+        while(i<str.length){
+            let j=i
+            while(str[j]!=='#'){
+                j++;
+            }
+            let length = parseInt(str.slice(i,j),10)
+
+            let start = j+1
+            let end = start + length
+            res.push(str.slice(start,end))
+
+            i = end
+        }
+        return res
+    }
+}
